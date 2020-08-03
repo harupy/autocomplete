c0ac267fad3cda104415c60014c5d32f9f994393
harupy <17039389+harupy@users.noreply.github.com>
2020-08-03 22:52:45 +0900

    Fix url pattern

--- src/content_script.ts
+++ src/content_script.ts
@@ -3,6 +3,7 @@ import { MAIN_SCRIPT_PATH } from './constants';
 const s = document.createElement('script');
 s.src = chrome.runtime.getURL(MAIN_SCRIPT_PATH);
 s.onload = () => {
+  console.log();
   s.remove();
 };
 (document.head || document.documentElement).appendChild(s);
