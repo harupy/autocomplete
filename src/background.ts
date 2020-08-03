220a05d58e53833e70f3ec88c690d2fabc8c33df
harupy <17039389+harupy@users.noreply.github.com>
2020-08-03 23:09:16 +0900

    Add condition to prevent errors

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,8 @@
 import { MAIN_SCRIPT_PATH } from './constants';
+import { isPullRequestUrl } from './utils';
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function () {
-  chrome.tabs.executeScript(null, { file: MAIN_SCRIPT_PATH });
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+  if (isPullRequestUrl(details.url)) {
+    chrome.tabs.executeScript(null, { file: MAIN_SCRIPT_PATH });
+  }
 });
