70371fc7dbf748bc5576f1579cddea78635bdc64
harupy <17039389+harupy@users.noreply.github.com>
2020-08-03 23:18:46 +0900

    use-arrow

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
 import { MAIN_SCRIPT_PATH } from './constants';
 import { isPullRequestUrl } from './utils';
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
   if (isPullRequestUrl(details.url)) {
     chrome.tabs.executeScript(null, { file: MAIN_SCRIPT_PATH });
   }
